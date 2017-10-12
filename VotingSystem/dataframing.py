b175469b79218eff9305ecc5f725f35ecb761787
Janeseed <tjdus0515@gmail.com>
2017-10-12 18:05:48 +0900

    Mainly fixed Errorcheck function and Add the case of different classes. 1. At Errorcheck function, delete the if loop counting the length of index Just check if the students whether vote same answers twice. 2. Reorganize the code for different classes. Fixing main mistake at first checking file path. reorganize the code of getting dataframe of each classes and rewrite on excel file.

--- VotingSystem/dataframing.py
+++ VotingSystem/dataframing.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import xlrd
+from openpyxl import load_workbook
+import re
 
 def ErrorCheck(df_vote):
     ## checking the mistaken of inputs
@@ -22,42 +24,41 @@ def ErrorCheck(df_vote):
     for sample in df_sample:
         # get indexes of same student id
         li_index = df_sample.get(sample)
-        #print(li_index)
+        print(li_index)
         # Find if the number of indexes are more than 3.
         # Remove the double vote data.
-        if len(li_index) >= 3:
-            li_1st = []
-            li_2nd = []
-            for i in range(len(li_index)):
-                # get the value of first and seconde vote.
-                row = df_vote.ix[li_index[i]]
-                first_vote = row["Let's vote."]
-                second_vote = row["Let's vote again."]
-                if not np.isnan(first_vote):
-                    li_1st.append({li_index[i]: first_vote})
-                if not np.isnan(second_vote):
-                    li_2nd.append({li_index[i] : second_vote})
-            print(li_1st, li_2nd)
-            # the print results are like below
-            # example1. ([{13: 3.0}], [{128: 3.0}, {129: 3.0}])
-            # exampel2. ([{5: 4.0}, {11: 4.0}], [{5: 4.0}, {92: 4.0}])
-            if len(li_1st) > 1:
-                if li_1st[0].values() == li_1st[1].values():
-                    random_index = random.choice(li_1st).keys()[0]
-                    print(random_index)
-                    if random_index in df_vote.index:
-                        df_vote = df_vote.drop(random_index)
-                        print(random_index, "is deleted")
+        li_1st = []
+        li_2nd = []
+        for i in range(len(li_index)):
+            # get the value of first and seconde vote.
+            row = df_vote.ix[li_index[i]]
+            first_vote = row["Let's vote."]
+            second_vote = row["Let's vote again."]
+            if not np.isnan(first_vote):
+                li_1st.append({li_index[i]: first_vote})
+            if not np.isnan(second_vote):
+                li_2nd.append({li_index[i] : second_vote})
+        print(li_1st, li_2nd)
+        # the print results are like below
+        # example1. ([{13: 3.0}], [{128: 3.0}, {129: 3.0}])
+        # exampel2. ([{5: 4.0}, {11: 4.0}], [{5: 4.0}, {92: 4.0}])
 
-            if len(li_2nd) > 1:
-                if li_2nd[0].values() == li_2nd[1].values():
-                    random_index = random.choice(li_2nd).keys()[0]
-                    print(random_index)
-                    if random_index in df_vote.index:
-                        df_vote = df_vote.drop(random_index)
-                        print(random_index, "is deleted")
-                    else:
-                        pass
+        # If it has double voting data and the value is same, deleted
+        if len(li_1st) > 1:
+            if li_1st[0].values() == li_1st[1].values():
+                random_index = random.choice(li_1st).keys()[0]
+                print(random_index)
+                # Check if the dataframe has the data you want to deleted.
+                if random_index in df_vote.index:
+                    df_vote = df_vote.drop(random_index)
+                    print(random_index, "is deleted")
+        if len(li_2nd) > 1:
+            if li_2nd[0].values() == li_2nd[1].values():
+                random_index = random.choice(li_2nd).keys()[0]
+                print(random_index)
+                if random_index in df_vote.index:
+                    df_vote = df_vote.drop(random_index)
+                    print(random_index, "is deleted")
     # Do the students put their 'student ID' correctly?
     #                           'Name'       correctly?
     return df_vote
@@ -79,7 +80,7 @@ if __name__ =='__main__':
                 df_class = pd.read_excel(file_path, i)
                 if df_class.any : # have to fix later. but it works on 'EM_Fall_2017.xlsx'
                     print('Session '+ str(i) +'is opened correctly')
-                df_list.append(df_original)
+                df_list.append(df_class)
             input_path = sys.argv[3]
             if(os.path.isfile(input_path)==False):
                 print("Error: the file specified does not exist")
@@ -91,49 +92,67 @@ if __name__ =='__main__':
     #Check the errors and remove it before append to student roster.
     df_vote = ErrorCheck(df_vote)
 
-    ##Sample one of the line and matching the students in df_vote to df_list
+    ## make a new series for append to the dataframe finally
+    #Sample one of the row and matching the students in df_vote(dataframe of voting values)
+    #                                                to df_list(dataframe of student roster)
     first_vote = pd.DataFrame()
     second_vote = pd.DataFrame()
+    print(sys.argv[3])
+    if str(sys.argv[3]).startswith('Vote_41'):
+        print('41')
+        df_4n = df_list[0]
+    elif str(sys.argv[3]).startswith('Vote_42'):
+        print('42')
+        df_4n = df_list[1]
+    elif str(sys.argv[3]).startswith('Vote_43'):
+        print('43')
+        df_4n = df_list[2]
+    elif str(sys.argv[3]).startswith('Vote_44'):
+        print('44')
+        df_4n = df_list[3]
+
+    print(df_4n)
     for i in range(df_vote.shape[0]):
+        # sample one of the row and get student ID of it
         sample = df_vote.sample(n=1)
         student_num = sample[u"Student ID No. (학번)"].values[0]
-
         sample = sample.dropna(axis = 1)
-        #print(sample)
 
-        #print(sample["Let's vote."].values)
-        #print(sys.argv[3])
-        if sys.argv[3].startswith('Vote_41'):
-            df_4n = df_list[0]
-            idx = df_4n.index[df_4n['sid'] == student_num]
-            #print(sample.columns.values)
-        elif sys.argv[3].startswith('Vote_42'):
-            df_4n = df_list[1]
-            idx = df_4n.index[df_4n['sid'] == student_num]
-        elif sys.argv[3].startswith('Vote_43'):
-            df_4n = df_list[2]
-            idx = df_4n.index[df_4n['sid'] == student_num]
-        elif sys.argv[3].startswith('Vote_44'):
-            df_4n = df_list[3]
-            idx = df_4n.index[df_4n['sid'] == student_num]
-
-
+        idx = df_4n.index[df_4n['sid'] == student_num]
+        #print(idx)
         if sample.columns.values[-1] == u"Let's vote.":
             temp1 = pd.DataFrame(sample[u"Let's vote."].values[0], index = idx, columns= ['1st_QLA_15'])
+            #print(temp1)
             first_vote = first_vote.append(temp1)
         if sample.columns.values[-1] == u"Let's vote again.":
             temp2 = pd.DataFrame(sample[u"Let's vote again."].values[0], index = idx, columns=['2nd_QLA_15'])
+            #print(temp2)
             second_vote = second_vote.append(temp2)
             if sample.columns.values[-2] == u"Let's vote.":
-                temp2_1 = pd.Dataframe(sample[u"Let's vote."].values[0], index = idx, columns= ['1st_QLA_15'])
+                temp2_1 = pd.DataFrame(sample[u"Let's vote."].values[0], index = idx, columns= ['1st_QLA_15'])
+                #print(temp2_1)
+                first_vote = first_vote.append(temp2_1)
         #print(temp1)
         df_vote = df_vote.drop(sample.index)
-    #print(first_vote)
+    print(first_vote)
     # append the new colums of '1st_QLA_15', '2nd_QLA_15'
-    # make a new series for append to the dataframe finally
-    if not first_vote.empty:
-        df_4n = df_4n.merge(first_vote, left_index = True, right_index = True )
-    if not second_vote.empty:
-        df_4n = df_4n.merge(second_vote, left_index = True, right_index = True)
-    print(df_4n)
+    book = load_workbook(sys.argv[1])
+    writer = pd.ExcelWriter(sys.argv[1], engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    result = pd.concat([df_4n, first_vote], axis = 1)
+    result = pd.concat([result, second_vote], axis = 1)
+
+    if sys.argv[3].startswith('Vote_41'):
+        result.to_excel(writer,"EM_41")
+    elif sys.argv[3].startswith('Vote_42'):
+        result.to_excel(writer,"EM_42")
+    elif sys.argv[3].startswith('Vote_43'):
+        result.to_excel(writer,"EM_43")
+    elif sys.argv[3].startswith('Vote_44'):
+        result.to_excel(writer,"EM_44")
+    writer.save()
+    #print(result)
+
     # matching the answer by student ID.
